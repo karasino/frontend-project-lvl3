dcf91c40608150a50da64fc0ebd1c7f47044cfc9
Big Gun <karasino@mail.ru>
2021-05-06 17:25:02 +0300

    Add soem logic from step 2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,59 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import * as yup from 'yup';
+import axios from 'axios';
 
-const generateForm = () => {
-  const form = document.createElement('form');
-  const input = document.createElement('input');
-  const submit = document.createElement('input');
-  submit.setAttribute('type', 'submit');
-  form.appendChild(input);
-  form.appendChild(submit);
+const state = {
+  form: {
+    state: 'sent',
+    urlList: [],
+  },
+  channels: [],
+  posts: [],
 };
 
-document.body.appendChild(generateForm());
+const form = document.createElement('form');
+const formGroup = document.createElement('div');
+formGroup.classList.add('form-group');
+const label = document.createElement('label');
+label.setAttribute('for', 'inputUrl');
+label.textContent = 'URL to add';
+const input = document.createElement('input');
+input.setAttribute('name', 'input');
+input.classList.add('form-control');
+input.setAttribute('id', 'inputUrl');
+const submit = document.createElement('button');
+submit.setAttribute('type', 'submit');
+submit.classList.add('btn', 'btn-primary');
+submit.textContent = 'ADD';
+formGroup.append(label, input);
+form.append(formGroup, submit);
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const schema = yup.string().url();
+  const url = e.target.elements.input.value;
+  if (state.form.urlList.includes(url)) {
+    state.form.state = 'invalid';
+  }
+  schema.isValid(url)
+    .catch(() => {
+      state.form.state = 'invalid';
+    })
+    .then((validUrl) => {
+      state.form.state = 'sending';
+      axios.get(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(validUrl)}`);
+    })
+    .then((response) => {
+      if (response.ok) return response.json();
+      throw new Error('Network response was not ok.');
+    })
+    .catch(() => {
+      state.form.state = 'network problem';
+    })
+    .then((data) => {
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(data, 'application/xml');
+    });
+});
+
+document.body.appendChild(form);
