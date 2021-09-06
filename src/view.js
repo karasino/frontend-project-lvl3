a78f13e304ab7f382f3b766005c2cff6ff98b3df
Big Gun <karasino@mail.ru>
2021-09-06 20:22:04 +0300

    Complete data to unknown error

--- src/view.js
+++ src/view.js
@@ -96,7 +96,7 @@ const renderFormSending = (addFeedProcess, i18n, formEl, feedback) => {
     case 'ready':
       break;
     default:
-      throw new Error('unknown error');
+      throw new Error(addFeedProcess.status);
   }
 };
 
